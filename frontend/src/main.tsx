c692d3bfa2722071b48dffce351fc01f8fc496df
Francisco Ochoa Rodríguez <96065@sistemas.frc.utn.edu.ar>
2026-03-16 20:26:48 -0300

    fix: arreglo id hardcodeado

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -6,7 +6,7 @@ import { GoogleOAuthProvider } from '@react-oauth/google'
 import './index.css'
 import App from './App.tsx'
 
-const GOOGLE_CLIENT_ID = '877910786750-9tprg2pqpl1s9tu93h988pbk031jc4ht.apps.googleusercontent.com';
+const GOOGLE_CLIENT_ID = import.meta.env.VITE_GOOGLE_CLIENT_ID;
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
